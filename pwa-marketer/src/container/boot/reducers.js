274cf0a9acc33bd63c11146bce378af56e82afbf
MeysamNuri <65802911+MeysamNuri@users.noreply.github.com>
2021-01-05 00:49:24 -0800

    addd

--- pwa-marketer/src/container/boot/reducers.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from 'redux';
-import FormReducer from '../Reducers/FormReducer'
-import Token from '../Reducers/Token'
-import snackbarReducer from '../Reducers/snackbarReducer';
-import insuranceReducer from '../components/insuranceModal/reducer'
-
-export default combineReducers({ FormReducer, Token, snackbarReducer, insuranceReducer });
--- pwa-marketer/src/container/components/bank-list/API.js
+++
@@ -1,9 +0,0 @@
-import { axiosInstance as axios } from '../../config/axios';
-
-const API = {
-   async bankList() {
-      const { data } = await axios.get('marketer/banks/list');
-      return data;
-   }
-}
-export default API;
