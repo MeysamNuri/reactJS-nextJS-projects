1eb322b3a7f864b53c0fe352e2bf75362dfb2d27
meysam nuri <meysamnuri1992@gmail.com>
2024-11-13 18:23:44 +0330

    remove

--- loan project/src/boot/reducers.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import TabReducer from './../components/reducer'
-import ExpandReducer from './../components/LoanList/reducer'
-
-export default combineReducers({TabReducer,ExpandReducer });
